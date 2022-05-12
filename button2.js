788858e90d5f7b49b66669c2558ad1bb5e5ae497
KasperMoliin <moliin@hotmail.com>
2022-05-12 12:06:44 +0200

    content copied from index

--- button2.js
+++ button2.js
@@ -1,29 +1,30 @@
-fetch(`https://the-one-api.dev/v2/${value}`;
+fetch(`https://the-one-api.dev/v2/${value}`);
 
 const handleClick = () => {
-    const ulItem = document.getElementById("info-list");
-    ulItem.innerHTML = "";
+    console.log("Hallo");
+    // const ulItem = document.getElementById("info-list");
+    // ulItem.innerHTML = "";
   
-    let select = document.getElementById("dropdown");
-    let value = select.options[select.selectedIndex].value;
+    // let select = document.getElementById("dropdown");
+    // let value = select.options[select.selectedIndex].value;
   
-    fetch(`https://the-one-api.dev/v2/${value}`, {
-      headers: {
-        Authorization: "Bearer " + "F3Y4z5i4JLiNiRv2zfYI", // <-- moest nog een spatie na Bearer
-      },
-    }).then((JSONdata) => {
-      JSONdata.json().then((data) => {
-        console.log(data.docs);
+    // fetch(`https://the-one-api.dev/v2/${value}`, {
+    //   headers: {
+    //     Authorization: "Bearer " + "F3Y4z5i4JLiNiRv2zfYI", // <-- moest nog een spatie na Bearer
+    //   },
+    // }).then((JSONdata) => {
+    //   JSONdata.json().then((data) => {
+    //     console.log(data.docs);
   
-        data.docs.forEach((book) => {
-          const newListItem = document.createElement("li");
-          // newListItem.innerText(book); waarom werk dit niet?
+    //     data.docs.forEach((character) => {
+    //       const newListItem = document.createElement("li");
+    //       // newListItem.innerText(book); waarom werk dit niet?
   
-          newListItem.innerHTML = book.name;
-          ulItem.appendChild(newListItem);
-        });
-      });
-    });
+    //       newListItem.innerHTML = character.name;
+    //       ulItem.appendChild(newListItem);
+    //     });
+    //   });
+    // });
   };
   
 
